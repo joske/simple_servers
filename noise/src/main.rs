211eafe6b28f5c6b504a0c82425fbc79716429f0
Jos Dehaes <jos.dehaes@gmail.com>
2023-01-03 21:34:53 +0100

    use tokio for all

--- noise/src/main.rs
+++ noise/src/main.rs
@@ -1,11 +1,11 @@
 use lazy_static::lazy_static;
-use chrono::Utc;
 use clap::App;
 use snow::params::NoiseParams;
 use snow::Builder;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 use std::error::Error;
-use std::io::{self, Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::net::SocketAddr;
 
 static SECRET: &'static [u8] = b"XTSPPFrCk7sZmBFm8Hm6cXjjS7Ddd3PV";
 lazy_static! {
@@ -13,7 +13,7 @@ lazy_static! {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     let matches = App::new("simple")
         .args_from_usage("-s --server 'server mode'")
         .get_matches();
@@ -21,19 +21,20 @@ async fn main() {
     if matches.is_present("server") {
         run_server().await;
     } else {
-        run_client();
+        run_client().await?;
     }
     println!("all done.");
+    Ok(())
 }
 
 async fn run_server() {
     // wait on client's arrival
     println!("Listening on 0.0.0.0:9999");
-    let tcp_listener = TcpListener::bind("0.0.0.0:9999").unwrap();
+    let tcp_listener = TcpListener::bind("0.0.0.0:9999").await.unwrap();
     loop {
-        let (stream, _) = tcp_listener.accept().unwrap();
+        let (stream, addr) = tcp_listener.accept().await.unwrap();
         tokio::spawn(async move {
-            handle_client(stream)
+            handle_client(stream, addr)
                 .await
                 .map_err(|e| eprintln!("error: {:?}", e))
                 .ok();
@@ -41,8 +42,8 @@ async fn run_server() {
     }
 }
 
-async fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
-    println!("{:?} handling client", Utc::now());
+async fn handle_client(mut stream: TcpStream, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
+    println!("handling client from {:?}", addr);
     let mut buf = vec![0u8; 1024];
     // initialize responder
     let builder: Builder<'_> = Builder::new(PARAMS.clone());
@@ -52,26 +53,26 @@ async fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
         .psk(3, SECRET)
         .build_responder()?;
     // <- e
-    noise.read_message(&recv(&mut stream)?, &mut buf)?;
+    noise.read_message(&recv(&mut stream).await?, &mut buf)?;
     // -> e, ee, s, es
     let len = noise.write_message(&[0u8; 0], &mut buf)?;
-    send(&mut stream, &buf[..len]);
+    send(&mut stream, &buf[..len]).await?;
 
     // <- s, se
-    noise.read_message(&recv(&mut stream)?, &mut buf)?;
+    noise.read_message(&recv(&mut stream).await?, &mut buf)?;
     // transition the state machine to transport mode sinc handshake is complete.
     let mut noise = noise.into_transport_mode()?;
-    while let Ok(msg) = recv(&mut stream) {
+    while let Ok(msg) = recv(&mut stream).await {
         let len = noise.read_message(&msg, &mut buf)?;
         println!("client said: {}", String::from_utf8_lossy(&buf[..len]));
         let len = noise.write_message("Yes!".as_bytes(), &mut buf)?;
-        send(&mut stream, &buf[..len]);
+        send(&mut stream, &buf[..len]).await?;
     }
     println!("connection closed");
     Ok(())
 }
 
-fn run_client() {
+async fn run_client() -> Result<(), Box<dyn Error>>{
     let mut buf = vec![0u8; 1024];
 
     // initialize initiator
@@ -84,21 +85,21 @@ fn run_client() {
         .unwrap();
 
     // connect to server
-    let mut stream = TcpStream::connect("127.0.0.1:9999").unwrap();
+    let mut stream = TcpStream::connect("127.0.0.1:9999").await?;
     println!("connected!");
 
     // -> e
     let len = noise.write_message(&[], &mut buf).unwrap();
-    send(&mut stream, &buf[..len]);
+    send(&mut stream, &buf[..len]).await?;
 
     // <- e, ee, s, es
     noise
-        .read_message(&recv(&mut stream).unwrap(), &mut buf)
+        .read_message(&recv(&mut stream).await?, &mut buf)
         .unwrap();
 
     // -> s, se
     let len = noise.write_message(&[], &mut buf).unwrap();
-    send(&mut stream, &buf[..len]);
+    send(&mut stream, &buf[..len]).await?;
 
     let mut noise = noise.into_transport_mode().unwrap();
     println!("Session established...");
@@ -106,23 +107,25 @@ fn run_client() {
     // send secure data
     for _ in 0..10 {
         let len = noise.write_message(b"HACK THE PLANET", &mut buf).unwrap();
-        send(&mut stream, &buf[..len]);
+        send(&mut stream, &buf[..len]).await?;
     }
 
     println!("done!");
+    Ok(())
 }
 
-fn recv(stream: &mut TcpStream) -> io::Result<Vec<u8>> {
+async fn recv(stream: &mut TcpStream) -> std::io::Result<Vec<u8>> {
     let mut msg_len_buf = [0u8; 2];
-    stream.read_exact(&mut msg_len_buf)?;
+    stream.read_exact(&mut msg_len_buf).await?;
     let msg_len = ((msg_len_buf[0] as usize) << 8) + (msg_len_buf[1] as usize);
     let mut msg = vec![0u8; msg_len];
-    stream.read_exact(&mut msg[..])?;
+    stream.read_exact(&mut msg[..]).await?;
     Ok(msg)
 }
 
-fn send(stream: &mut TcpStream, buf: &[u8]) {
+async fn send(stream: &mut TcpStream, buf: &[u8]) -> std::io::Result<()> {
     let msg_len_buf = [(buf.len() >> 8) as u8, (buf.len() & 0xff) as u8];
-    stream.write_all(&msg_len_buf).unwrap();
-    stream.write_all(buf).unwrap();
+    stream.write_all(&msg_len_buf).await?;
+    stream.write_all(buf).await?;
+    Ok(())
 }
